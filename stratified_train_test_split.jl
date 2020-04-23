9286cdcf6a018d487be96c0c34f9a1cb82b1ebf5
Théo Lopès-Quintas <63822750+theo-lq@users.noreply.github.com>
2020-04-23 21:38:40 +0200

    Update stratified_train_test_split.jl

--- stratified_train_test_split.jl
+++ stratified_train_test_split.jl
@@ -1,4 +1,4 @@
-using DataFrames, StatsBase, Random
+using MLDataUtils
 
 
 
@@ -22,18 +22,6 @@ function stratified_train_test_split(df, y_column_number, target, train_size=0.7
     end
 
 
-    function split(X)
-        """Split a dataframe X into training and test set."""
-
-        n = nrow(X)
-        train_number = round(Int, n  * train_size)
-        index = sample(1:n, train_number, replace=false)
-        train = X[index, :]
-        test = X[Not(index), :]
-        return train, test
-    end
-
-
     function X_y_split(df)
         """Split a dataframe df into the feature array X and the target array y (perform binarisation on it)"""
 
@@ -44,28 +32,13 @@ function stratified_train_test_split(df, y_column_number, target, train_size=0.7
     end
 
 
-    function concatenate_shuffle_split(X1, X2)
-        """Concatenate the two dataframes X1 and X2, shuffle the rows and then split into X and y array"""
-        
-        answer = append!(X1, X2)
-        answer = answer[shuffle(1:nrow(answer)), :]
-        X, y = X_y_split(answer)
-        return X, y
-    end
-
-
 
     X_columns_number = [index for index in 1:size(df, 2)]
     remove!(X_columns_number, y_column_number)
 
-    df_target = df[(df[:, y_column_number] .== target), :]
-    df_not_target = df[(df[:, y_column_number] .!= target), :]
+    X, y = X_y_split(df)
 
-    train_target, test_target = split(df_target)
-    train_not_target, test_not_target = split(df_not_target)
-
-    X_train, y_train = concatenate_shuffle_split(train_target, train_not_target)
-    X_test, y_test = concatenate_shuffle_split(test_target, test_not_target)
+    (X_train, y_train), (X_test, y_test) = stratifiedobs((X, y), p=train_size)
 
     return Array(X_train), Array(X_test), Array(y_train), Array(y_test)
 end
